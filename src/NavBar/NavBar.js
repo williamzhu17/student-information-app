05ca32a095775c6ab5fc8ba49d52c62b1b69ea33
William Zhu <williammzhu@gmail.com>
2021-04-05 16:09:59 -0400

    added logo to navbar

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./NavBar.css";
+import logo from "./graph-logo-39x40.png";
+import logoText from "./logo-text-179x37.png"
 
 function NavBar() {
     return(
         <div>
-            <h1>e-results</h1>
+            <h1><img src={logo} alt="Blue bar graph" /><img src={logoText} alt="e-results" id="logoText" /></h1>
         </div>
     );
 }
