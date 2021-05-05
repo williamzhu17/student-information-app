f6740e2a287d314871949f055cad8efd96b1c265
William Zhu <williammzhu@gmail.com>
2021-05-05 14:29:49 -0400

    removed logo to navbar

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -6,7 +6,8 @@ import logoText from "./logo-text-179x37.png"
 function NavBar() {
     return(
         <div>
-            <h1><img src={logo} alt="Blue bar graph" /><img src={logoText} alt="e-results" id="logoText" /></h1>
+            <h1>e-results</h1>
+            {/* <h1><img src={logo} alt="Blue bar graph" /><img src={logoText} alt="e-results" id="logoText" /></h1> */}
         </div>
     );
 }
