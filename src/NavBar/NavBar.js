cb18a037e1c6100a97b10761712f1a61310e7e93
William Zhu <williammzhu@gmail.com>
2021-05-05 14:44:28 -0400

    Reverting to 5525382

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -5,7 +5,6 @@ function NavBar() {
     return(
         <div>
             <h1>e-results</h1>
-            {/* <h1><img src={logo} alt="Blue bar graph" /><img src={logoText} alt="e-results" id="logoText" /></h1> */}
         </div>
     );
 }
